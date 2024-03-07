07d9d919754d868d3b233cfb47f82d35f193acb1
sahilsheth05 <sahilnsheth@gmail.com>
2024-03-07 11:01:56 -0800

    fixed transition using button

--- Assets/Scenes/Menu/MainMenu.cs
+++ Assets/Scenes/Menu/MainMenu.cs
@@ -5,11 +5,34 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+    public Animator transition;
+
+    public float transitionTime = 1f;
+
     public void PlayGame()
     {
-
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        LoadNextLevel();
+        //SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
+
+
+    public void LoadNextLevel() 
+    {
+        StartCoroutine(LoadLevel(SceneManager.GetActiveScene().buildIndex + 1));
+    }
+
+
+    IEnumerator LoadLevel(int levelIndex)
+    {
+        //  Play animation
+        transition.SetTrigger("Start");
+        //  Wait
+        yield return new WaitForSeconds(transitionTime);
+        //  Load Scene
+        SceneManager.LoadScene(levelIndex);
+
+    }
+
 
     public void QuitGame()
     {
