359f7730a1b54bfed6e86d6a8b6595627df07546
sahilsheth05 <sahilnsheth@gmail.com>
2024-03-13 01:39:56 -0700

    game audio

--- Assets/Scripts/VolumeController.cs
+++ Assets/Scripts/VolumeController.cs
@@ -6,26 +6,31 @@ using TMPro;
 
 public class VolumeController : MonoBehaviour
 {
+    [SerializeField] private Slider volumeSlider = null;
+
+    [SerializeField] private TextMeshProUGUI volumeTextUI = null;
+    
     // Start is called before the first frame update
     void Start()
     {
-        volumeSlider.value = 50;
-        AudioListener.volume = volumeSlider.value;
+        if (volumeSlider != null) {
+            volumeSlider.value = 100;
+            Debug.Log(volumeSlider);
+            AudioListener.volume = volumeSlider.value/100;
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        AudioListener.volume = volumeSlider.value;
+        if (volumeSlider != null) {
+            AudioListener.volume = volumeSlider.value/100;
+            Debug.Log(volumeSlider);
+        }
     }
-
-    [SerializeField] private Slider volumeSlider = null;
-
-    [SerializeField] private TextMeshProUGUI volumeTextUI = null;
 
     public void VolumeSlider(float volume)
     {
-
         volumeTextUI.text = volume.ToString("0");
     }
 }
